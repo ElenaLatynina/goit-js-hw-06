064ececbd5b674bd743a3d81ffaf45f726a84837
ElenaLatynina <latynina.elena.v@gmail.com>
2022-07-18 13:12:12 +0300

    task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,19 +1,8 @@
-// const listCategories = document.querySelector(`#categories`);
-// console.log(listCategories);
+const listItemsEl = document.querySelectorAll(`.item`);
+console.log(`Number of categories: `, listItemsEl.length);
 
-const listItems = document.querySelectorAll(`li.item`);
-console.log(`Number of categories: `, listItems.length);
 
-// const itemEl = listItems.firstChild;
-// console.log(listItems[1].firstChild);
-
-const titlesOfEl = document.querySelectorAll(`h2`);
-
-const categoryTitlesEl = titlesOfEl.forEach(element => {
-    console.log(`Category: ${element.textContent}`);
-
-    const categoryItemsEl = element.lastElementChild;
-
-    const amountOfEl = document.querySelectorAll(`li`);
-    console.log(`Elements: ${categoryItemsEl.length}`);
+const categoriesEl = listItemsEl.forEach(element => {
+    console.log(`Category: ${element.firstElementChild.textContent}`);
+    console.log(`Elements: ${element.lastElementChild.children.length}`)
 })
