31439afc39fdc6f2319aca52c8c30cd6358aaae5
ElenaLatynina <latynina.elena.v@gmail.com>
2022-07-21 17:36:26 +0300

    task 5

--- js/task-05.js
+++ js/task-05.js
@@ -10,7 +10,6 @@ const nameOutput = document.querySelector(`#name-output`);
 nameInput.addEventListener(`input`, onInputChange )
 
 function onInputChange(event) {
-    console.log(event.currentTarget.value);
 
     if (event.currentTarget.value !== ``) {
         nameOutput.textContent = event.currentTarget.value;
